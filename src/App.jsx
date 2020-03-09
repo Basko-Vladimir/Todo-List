1fe559f94ef7a356825341ba0b20c0694757f9b9
Basko-Vladimir <1989bvg@gmail.com>
2020-03-09 19:31:18 +0300

    Step 02 - props

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import TodoListHeader from "./Components/TodoListHeader";
+import TodoListTasks from "./Components/TodoListTasks/TodoListTasks";
+import TodoListFooter from "./Components/TodoListFooter";
+
+class App extends React.Component {
+    tasks = [
+        {title: 'CSS', isDone: true, priority: 'low'},
+        {title: 'JS', isDone: false, priority: 'medium'},
+        {title: 'ReactJS', isDone: false, priority: 'high'},
+        {title: 'Patterns', isDone: true, priority: 'high'}
+    ];
+
+    filterValue = 'Active';
+    render = () => {
+        return (
+            <div className="App">
+                <div className="todoList">
+                    <TodoListHeader/>
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter filterValue={this.filterValue}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
--- src/Components/TodoListFooter.jsx
+++ src/Components/TodoListFooter.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 class TodoListFooter extends React.Component{
+
+    classForAll = this.props.filterValue === 'All' ? 'filter-active' : '';
+    classForCompleted = this.props.filterValue === 'Completed' ? 'filter-active' : '';
+    classForActive = this.props.filterValue === 'Active' ? 'filter-active' : '';
+
     render = () => {
         return (
             <div className="todoList-footer">
-                <button>All</button>
-                <button>Completed</button>
-                <button>Active</button>
+                <button className={this.classForAll}>All</button>
+                <button className={this.classForCompleted}>Completed</button>
+                <button className={this.classForActive}>Active</button>
             </div>
         )
     }
