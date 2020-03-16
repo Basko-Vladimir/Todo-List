bc170ab6658ea24c610d74da81eecd590a8a99e0
Basko-Vladimir <1989bvg@gmail.com>
2020-03-16 19:22:43 +0300

    Step 03 - Done task lesson 3 (React.createRef())

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,37 @@ import TodoListTasks from "./Components/TodoListTasks/TodoListTasks";
 import TodoListFooter from "./Components/TodoListFooter";
 
 class App extends React.Component {
-    tasks = [
-        {title: 'CSS', isDone: true, priority: 'low'},
-        {title: 'JS', isDone: false, priority: 'medium'},
-        {title: 'ReactJS', isDone: false, priority: 'high'},
-        {title: 'Patterns', isDone: true, priority: 'high'}
-    ];
 
-    filterValue = 'Completed';
+    constructor(props) {
+        super(props);
+        this.newTaskTitleRef = React.createRef();
+    }
+
+    onAddTaskClick = () => {
+        let newText = this.newTaskTitleRef.current.value;
+        let newTask = {title: newText, isDone: true, priority: 'medium'};
+        let newTasks = [...this.state.tasks, newTask];
+        this.setState({tasks: newTasks});
+        this.newTaskTitleRef.current.value = '';
+    };
+
+    state = {
+        tasks: [
+            {title: 'CSS', isDone: true, priority: 'low'},
+            {title: 'JS', isDone: false, priority: 'medium'},
+            {title: 'ReactJS', isDone: false, priority: 'high'},
+            {title: 'Patterns', isDone: true, priority: 'high'}
+        ],
+        filterValue: 'Completed'
+    };
+
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader/>
-                    <TodoListTasks tasks={this.tasks}/>
-                    <TodoListFooter filterValue={this.filterValue}/>
+                    <TodoListHeader newTaskTitRef={this.newTaskTitleRef} onAddTaskClick={this.onAddTaskClick} />
+                    <TodoListTasks tasks={this.state.tasks}/>
+                    <TodoListFooter filterValue={this.state.filterValue}/>
                 </div>
             </div>
         );
